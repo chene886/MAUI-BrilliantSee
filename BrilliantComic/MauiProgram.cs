95a6c428c840ec3bc819b5f8a477fd93f89fb817
chene <3256366564@qq.com>
2024-03-05 20:59:19 +0800

    优化基础图标启动图片

--- BrilliantComic/MauiProgram.cs
+++ BrilliantComic/MauiProgram.cs
@@ -12,12 +12,12 @@ namespace BrilliantComic
         {
             var builder = MauiApp.CreateBuilder();
             builder.UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                })
-                .UseMauiCommunityToolkit();
+                });
 
             // 添加服务
             AddServices(builder.Services);
