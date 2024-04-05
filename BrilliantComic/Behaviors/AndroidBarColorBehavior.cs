6e633f32184f88f6a706b9c16f1c5877b7113214
chene <3256366564@qq.com>
2024-04-06 00:05:50 +0800

    优化部分样式显示

--- BrilliantComic/Behaviors/AndroidBarColorBehavior.cs
+++ BrilliantComic/Behaviors/AndroidBarColorBehavior.cs
@@ -2,36 +2,27 @@
 
 namespace BrilliantComic.Behaviors
 {
-    internal class AndroidFullscreenBehavior : Behavior<Page>
+    internal class AndroidBarColorBehavior : Behavior<Page>
     {
         protected override void OnAttachedTo(Page page)
         {
             base.OnAttachedTo(page);
-            SetFullscreen(false);
-            page.Disappearing += (s, e) => SetFullscreen(true);
+            SetColor();
         }
 
-        private static void SetFullscreen(bool isVisible)
+        private static void SetColor()
         {
 #if ANDROID
             var activity = Platform.CurrentActivity ?? throw new InvalidOperationException("Android Activity can't be null.");
             var window = activity.Window ?? throw new InvalidOperationException($"{nameof(activity.Window)} cannot be null");
 #endif
-            if (isVisible)
-            {
+
 #if ANDROID
                 window!.DecorView.SystemUiFlags = Android.Views.SystemUiFlags.LightStatusBar;
                 window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#FAFAFA"));
                 window!.DecorView.SystemUiFlags |= Android.Views.SystemUiFlags.LightNavigationBar;
                 window.SetNavigationBarColor(Android.Graphics.Color.ParseColor("#FAFAFA"));
 #endif
-            }
-            else
-            {
-#if ANDROID
-                window!.DecorView.SystemUiFlags = (Android.Views.SystemUiFlags.Fullscreen | Android.Views.SystemUiFlags.HideNavigation | Android.Views.SystemUiFlags.ImmersiveSticky | Android.Views.SystemUiFlags.LayoutFullscreen | Android.Views.SystemUiFlags.LayoutHideNavigation);
-#endif
-            }
         }
     }
 }
